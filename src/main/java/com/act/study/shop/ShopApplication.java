ea307eacd124233562931714946550308880bb7d
kjspo56 <kjspo56@gmail.com>
2022-05-30 23:42:28 +0900

    basic crud구성

--- src/main/java/com/act/study/shop/ShopApplication.java
+++ src/main/java/com/act/study/shop/ShopApplication.java
@@ -2,15 +2,21 @@ package com.act.study.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class ShopApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 
 }
