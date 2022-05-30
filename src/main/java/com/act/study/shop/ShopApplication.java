5242c666fd506f0178147944b4d039b84c961508
kjspo56 <kjspo56@gmail.com>
2022-05-30 22:15:44 +0900

    Initial commit

---
+++ src/main/java/com/act/study/shop/ShopApplication.java
@@ -0,0 +1,16 @@
+package com.act.study.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
